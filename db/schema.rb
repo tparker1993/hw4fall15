d3d2e5057042c0f3435040cc63e82d3460af6e54
tparker19931 <tyler-parker@uiowa.edu>
2015-10-05 23:56:20 +0000

    Part 1 -Database updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20151005233754) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.string   "rating"
     t.text     "description"
     t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "email"
+    t.string   "session_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

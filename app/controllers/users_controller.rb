6f1bd0e2164ad4e338aea54cebe0f236bed887e8
tparker19931 <tyler-parker@uiowa.edu>
2015-10-06 02:23:33 +0000

    Adding create for sign up, added unique check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def create
         @user = User.create(user_params)
-        flash[:notice] = "Welcome #{:user_id} Your account has been created"
+        flash[:notice] = "Welcome #{@user.user_id} Your account has been created"
         redirect_to movies_path
   end
 end

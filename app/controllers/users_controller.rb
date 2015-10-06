3ca342c073da6d1276ebf2adb4b26509eeb644e7
tparker19931 <tyler-parker@uiowa.edu>
2015-10-06 01:01:46 +0000

    Adding create for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     if Movie.find params[:user_id] != nil
       @user = User.create!(user_params)
       flash[:notice] = "Welcome #{@user.user_id} Your account has been created"
-      redirect_to movies_path
+      redirect_to users_path
     else
       flash[:notice] = "Sorry, this user-id is taken. Try again."
       redirect_to new_user_path

32880806918aea5644a25c48c202d0511aa9faa8
tparker19931 <tyler-parker@uiowa.edu>
2015-10-06 01:24:24 +0000

    Adding create for sign up, added user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
    
   def user_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:user).permit(:user_id, :email)
   end
   
   def new
